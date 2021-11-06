a9b5e1099ecc5c61983fb2364faf060610f45227
Egor <egor_chek@mail.ru>
2021-11-06 09:43:41 +0400

    Complete lesson project. Chat  with Firebase

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by admin on 14.08.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
